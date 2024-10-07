3b9fc867c1dcdd2140a77379628af174fb5a6ff1
Arowolo Ebine <79418698+Arosebine@users.noreply.github.com>
2024-10-07 19:36:17 +0100

    Update app.js  removed a module

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
 const morgan = require('morgan');
 
 
